63aad0fbe38f77c96b258d811fcbab0327581557
Nikhil Nanda <nikhilnanda02@gmail.com>
2020-02-13 12:51:58 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 window.onload = () => {
-  'use strict';
+    'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js');
+    }
+}
+window.onDeviceReady: function () {
+    //this.receivedEvent('deviceready');
+    $("#loginBtn").click($.proxy(function (event) {
+        if ($('#username').val() == '') {
+            tempHTML = "<label  class='errorLabel error-alert error-alert-login'>Username cannot be blank</label>";
+            $('#username').after(tempHTML);
+        }
+
+
+        if ($('#password').val() == '') {
+            tempHTML = "<label class='errorLabel error-alert error-alert-login' >Password cannot be blank</label>";
+            $('#password').after(tempHTML);
+        }
+
+        if ($('#username').val() != '' && $('#password').val() != '') {
+            $('#loginDiv').hide();
+            $('#dashboardDiv').show();
+            $('#welcomeMessage').append($('#username').val());
+        }
+    }));
+
 }
--- sw.js
+++ sw.js
@@ -1,25 +1,26 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'hello-pwa:v1';
 var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+    '/images/img_avatar2.png'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
