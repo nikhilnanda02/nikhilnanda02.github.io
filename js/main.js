e24ad87f93178d4a525de6a2cd6406301963d0bf
Nikhil Nanda <nikhil.nanda@newgensoft.com>
2024-07-04 13:26:32 +0530

    Update main.js
    update

--- js/main.js
+++ js/main.js
@@ -32,12 +32,17 @@ window.onload = () => {
                 // } catch(err) {
                 //     console.log('The native context does not exist yet');
                 // }    
-
-            var data = NEMF.invokeThirdPartyService($('#username').val());
+                var invokeThirdPartyServiceOptions = {};
+                invokeThirdPartyServiceOptions.method = "MORPHO_FINGERPRINT";
+                invokeThirdPartyServiceOptions.intentAction = "in.gov.uidai.rdservice.fp.CAPTURE";
+                invokeThirdPartyServiceOptions.packageToOpen = "com.idemia.l1rdservice";
+                invokeThirdPartyServiceOptions.PID_OPTIONS = "<PidOptions ver=\"1.0\"><Opts env=\"PP\" fCount=\"1\" fType=\"2\" iCount=\"0\" iType=\"\" pCount=\"0\" pType=\"\" format=\"0\" pidVer=\"2.0\" timeout=\"20000\" wadh=\"E0jzJ/P8UopUHAieZn8CKqS4WPMi5ZSYXgfnlfkWjrc=\" posh=\"UNKNOWN\"/><Demo></Demo><CustOpts><Param name=\"\" value=\"\" /></CustOpts></PidOptions>";
+                NEMF.invokeThirdPartyService(invokeThirdPartyServiceOptions);
+            // var data = NEMF.invokeThirdPartyService($('#username').val());
             // var nativeCallbackHandler = function(params){
             //     console.log("recieved params ", params);
             // }
-            $('#password').val(data);
+            // $('#password').val(data);
             // $('#loginDiv').hide();
             // $('#dashboardDiv').show();
             // $('#welcomeMessage').append($('#username').val());
